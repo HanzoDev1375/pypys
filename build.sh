fb462cf28756f007f38978ec94d315860344546b
HanzoDev1375 <ninjacodermain1375@gmail.com>
2025-02-19 19:34:52 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,16 +5,7 @@ set -x
 
 THIS_DIR="$PWD"
 
-# Check Autoconf version
-REQUIRED_AUTOCONF_VERSION=2.71
-AUTOCONF_VERSION=$(autoconf --version | grep -oP '\d+\.\d+\.\d+' | head -n 1)
-
-if [[ "$(printf '%s\n' "$REQUIRED_AUTOCONF_VERSION" "$AUTOCONF_VERSION" | sort -V | head -n1)" != "$REQUIRED_AUTOCONF_VERSION" ]]; then
-    echo "Error: Autoconf version $REQUIRED_AUTOCONF_VERSION or higher is required."
-    exit 1
-fi
-
-PYVER=3.11.0
+PYVER=3.12.9
 SRCDIR=src/Python-$PYVER
 
 COMMON_ARGS="--arch ${ARCH:-arm} --api ${ANDROID_API:-23}"
@@ -23,6 +14,8 @@ if [ ! -d $SRCDIR ]; then
     mkdir -p src
     pushd src
     curl -vLO https://www.python.org/ftp/python/$PYVER/Python-$PYVER.tar.xz
+    # Use --no-same-owner so that files extracted are still owned by the
+    # running user in a rootless container
     tar --no-same-owner -xf Python-$PYVER.tar.xz
     popd
 fi
